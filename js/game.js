f4400cb1bdb5cfc8fb7ce4098d347059ad2942c8
gudu <nspforever@163.com>
2014-04-05 23:14:55 -0400

    SpriteSheet class

--- js/game.js
+++ js/game.js
@@ -1,6 +1,6 @@
 var canvas = document.getElementById("game");
-
 var ctx = canvas.getContext && canvas.getContext('2d');
+
 if(!ctx) {
     //No 2d context available
     alert("Your browser is too old, please upgrade it.");
@@ -9,21 +9,28 @@ else {
     startGame();
 }
 
+// new function for signleton
+
+
 function startGame() {
-    ctx.fillStyle = "#FFFF00";
-    ctx.fillRect(50, 100, 380, 400);
-    //semi-transparent blue rectangle
-    ctx.fillStyle = "rgba(0, 0, 128, 0.5)";
-    ctx.fillRect(0, 50, 380, 400);
+    var spriteMap = {
+        ship : {
+            sx: 0, 
+            sy: 0, 
+            w: 37, 
+            h: 42, 
+            frames: 3}
+    };
     
-    var img = new Image();
-    // Do not change the order of this callback
-    // and the set img.src or in some browser if
-    // the image is cached the callback won't be 
-    // triggered
-    img.onload = function() {
-        ctx.drawImage(img, 1, 0, 36, 42, 110, 110, 36, 42);
-    }
-    img.src = "images/sprites.png";
+    var callback = function() {
+        SpriteSheet.draw(ctx, "ship", 0, 0);
+        SpriteSheet.draw(ctx, "ship", 100, 50);
+        SpriteSheet.draw(ctx, "ship", 150, 100, 1);
+    };
+    
+    SpriteSheet.load(spriteMap, callback);
     
 }
+
+
+
